0475cec0d7ac7384f210731bcf09348797978e93
arantzagmp <arantza23gmp@gmail.com>
2026-01-18 13:33:43 -0600

    ajustiki

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App.jsx";
 
-import "./blocks/page.css";
-import "./blocks/header.css";
-import "./blocks/profile.css";
-import "./blocks/cards.css";
-import "./blocks/footer.css";
-import "./blocks/popup.css";
-import "./blocks/popupCreate.css";
-import "./blocks/popupImage.css";
-import "./blocks/PopupWithConfirmation.css";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
