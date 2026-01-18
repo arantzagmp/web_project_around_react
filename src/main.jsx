e1082565f40e1f9a3a948c140b2a70430b51105d
arantzagmp <arantza23gmp@gmail.com>
2026-01-18 11:47:08 -0600

    ajuste5

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./vendor/normalize.css";
 import "./vendor/fonts.css";
-import './index.css'; 
-// Project checklist expects these CSS blocks to be imported from JS (or index.css).
-import './blocks/page.css';
-import './blocks/popup.css';
-import './blocks/popupCreate.css';
-import './blocks/popupImage.css';
-import './blocks/header.css';
-import './blocks/profile.css';
-import './blocks/cards.css';
-import './blocks/footer.css';
-import './blocks/PopupWithConfirmation.css';
+import "./index.css"; 
 import App from './components/App'
 
 createRoot(document.getElementById('root')).render(
